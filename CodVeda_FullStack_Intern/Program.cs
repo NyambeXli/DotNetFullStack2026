f58e7a6f55ffc9856676783cd46e89a142677c4a
Lindele Nyambe <lxnyambe@gmail.com>
2026-03-23 16:30:22 +0200

    feat: implemented password structure validation and modernized Edit view

--- CodVeda_FullStack_Intern/Program.cs
+++ CodVeda_FullStack_Intern/Program.cs
@@ -1,11 +1,22 @@
 using CodVeda_FullStack_Intern.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 7; 
+    options.Password.RequiredUniqueChars = 0;
+});
 
 builder.Services.AddControllersWithViews();
 
